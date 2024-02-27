e2f6ba5bcdb39430dc1c42e7955bd1e1b8b0e161
skryabin-p <skryabin.p.n@gmail.com>
2024-02-27 10:16:52 +0300

    final

--- FlashLoan.sol
+++ FlashLoan.sol
@@ -6,10 +6,6 @@ import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAd
 import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 
 
-// 0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A IPoolAddressesProvider sepolia eth
-
-// 
-
 contract Flashloan is FlashLoanSimpleReceiverBase {
       
     address payable owner;
@@ -35,64 +31,44 @@ contract Flashloan is FlashLoanSimpleReceiverBase {
     
   }
 
-    function executeOperation(
-        address asset,
-        uint256 amount,
-        uint256 premium,
-        address initiator,
-        bytes calldata params
-  ) external override returns (bool) {
-        // we have the borrowed funds
-        // custom logic
+  function executeOperation(
+      address asset,
+      uint256 amount,
+      uint256 premium,
+      address initiator,
+      bytes calldata params
+) external override returns (bool) {
+      // we have the borrowed funds
 
-        (address[] memory routers, bytes[] memory trades, address[] memory tokens, bytes[] memory approves) = abi.decode(params, (address[], bytes[], address[], bytes[]));
-        // router adresses length must be eaqual to trades data length
-        require(routers.length > 0 && routers.length == trades.length, "Invalid input");
-        
-        for(uint256 i; i < routers.length; i++) {
-
-            // Approve each token 
-            (bool successApprove,bytes memory returnData ) = tokens[i].call{gas: gasleft()}(approves[i]);
-           //(bool successApprove,bytes memory returnData ) = tokens[i].call(
-             // abi.encodeWithSignature("approve(address,uint256)", routers[i], type(uint256).max));
-            //bool successApprove = IERC20(tokens[i]).approve(address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), type(uint256).max);
-            //require(false,"We can approve!!!!");
-            require(successApprove, "Can not approve");
-            //Send trade data to contract adresses   {value: address(this).balance, gas: gasleft()}
-            (bool success, bytes memory returndata) = routers[i].call{gas: gasleft()}(trades[i]);
-            require(success, string(returndata));
-            //revert("We finished at least one trade!!!!");
-        }
-
-        // Approve borrowed amount + premium for AAVE pool contract
-        uint256 amountOwed = amount + premium;
-        //revert("We finished trades!!!!");
-        IERC20(asset).approve(address(POOL), amountOwed);
-        return true;
-
-  }
-
-  function gas_left() public view returns (uint) {
-      return gasleft();
-  }
-
-  function viewParams(bytes calldata params) public pure returns(address[] memory, bytes[] memory, address[] memory, bytes[] memory) {
       (address[] memory routers, bytes[] memory trades, address[] memory tokens, bytes[] memory approves) = abi.decode(params, (address[], bytes[], address[], bytes[]));
-      return (routers, trades, tokens, approves);
-  }
+      // router adresses length must be eaqual to trades data length
+      require(routers.length > 0 && routers.length == trades.length, "Invalid input");
+      
+      for(uint256 i; i < routers.length; i++) {
+
+          // Approve each token 
+          (bool successApprove, ) = tokens[i].call{gas: gasleft()}(approves[i]);
+
+          require(successApprove, "Can not approve");
+          //Send trade data to contract adresses  
+          (bool success, bytes memory returndata) = routers[i].call{gas: gasleft()}(trades[i]);
+          require(success, string(returndata));
+          
+      }
+
+      // Approve borrowed amount + premium for AAVE pool contract
+      uint256 amountOwed = amount + premium;
+      //revert("We finished trades!!!!");
+      IERC20(asset).approve(address(POOL), amountOwed);
+      return true;
+
+}
+
 
   function getBalance(address _tokenAddress) external view returns (uint) {
     return IERC20(_tokenAddress).balanceOf(address(this));
   }
 
-  function encodeApproves(address token, uint256 amount) pure public  returns (bytes memory){
-      return abi.encodeWithSignature("approve(address,uint256)", token, amount);
-  }
-
-  function decodeApproves(bytes calldata approve) pure public returns(address, uint256){
-    (address token, uint256 amount) = abi.decode(approve, (address, uint256));
-    return (token, amount);
-  }
 
   modifier onlyOwner() {
         require(msg.sender==owner, "You are not the owner");
